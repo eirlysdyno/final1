b427e823a9edb26d835880c6a3403f2c1f5544b5
Ngoc Nguyen Duy <161008082+eirlysdyno@users.noreply.github.com>
2026-01-17 14:57:05 +0700

    Update chatbot.js

--- api/chatbot.js
+++ api/chatbot.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
       : "Dữ liệu ngân hàng MBV"
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.5-flash"
+      model: "gemini-1.5-flash"
     })
 
     const result = await model.generateContent(
