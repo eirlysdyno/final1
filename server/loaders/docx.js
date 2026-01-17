7ad57fcddb31d555489de79e2d6f61efb8259a0f
Ngoc Nguyen Duy <161008082+eirlysdyno@users.noreply.github.com>
2026-01-17 13:53:13 +0700

    Delete server directory
    Rename

--- server/loaders/docx.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs"
-import mammoth from "mammoth"
-
-export async function loadDocx(filePath) {
-  const buffer = fs.readFileSync(filePath)
-
-  const result = await mammoth.extractRawText({ buffer })
-  return result.value
-}
